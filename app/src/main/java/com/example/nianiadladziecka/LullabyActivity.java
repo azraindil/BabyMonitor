efe86393f959ce5946c2f49d8cc978d34f34e264
Marta Kowalczyk <azraindil@gmail.com>
2020-03-20 11:30:12 +0100

    add spinner

--- app/src/main/java/com/example/nianiadladziecka/LullabyActivity.java
+++ app/src/main/java/com/example/nianiadladziecka/LullabyActivity.java
@@ -4,15 +4,16 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
-public class LullabyActivity extends AppCompatActivity {
+public class LullabyActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Button btnStop,btnStart;
     Spinner spiLullaby;
-SpinnerAdapter adapter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
@@ -22,7 +23,10 @@ SpinnerAdapter adapter;
         btnStop = findViewById(R.id.btnStop);
         btnStart = findViewById(R.id.btnStart);
         spiLullaby = findViewById(R.id.spiLullaby);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.lullaby, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiLullaby.setAdapter(adapter);
+        spiLullaby.setOnItemSelectedListener(this);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,7 +34,7 @@ SpinnerAdapter adapter;
                     @Override
                     protected Void doInBackground(Integer... params) {
                         try {
-                            SShCommandSend.executeRemoteCommand("pi", "makova94", AppConfig.IP_RPI, 22, "omxplayer --no-keys -o local "+ spiLullaby.getSelectedItem());
+                            SShCommandSend.executeRemoteCommand("pi", "makova94", AppConfig.IP_RPI, 22, "omxplayer --no-keys -o local "+ spiLullaby.getSelectedItem().toString());
                         }
                         catch (Exception e){
                             Toast.makeText(getApplicationContext(),"Error: "+ e.getMessage(),Toast.LENGTH_LONG).show();
@@ -56,5 +60,15 @@ SpinnerAdapter adapter;
                 }.execute(1);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
