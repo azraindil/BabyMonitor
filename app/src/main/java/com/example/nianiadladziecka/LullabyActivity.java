3f702e3d485fccb2f91044f007509cf739fef462
Marta Kowalczyk <azraindil@gmail.com>
2020-03-20 13:06:19 +0100

    add spinner

--- app/src/main/java/com/example/nianiadladziecka/LullabyActivity.java
+++ app/src/main/java/com/example/nianiadladziecka/LullabyActivity.java
@@ -8,7 +8,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 public class LullabyActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
