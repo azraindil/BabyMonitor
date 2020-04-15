6cee6bf1ed0e392da560c4f9f5409912cd360675
Marta Kowalczyk <azraindil@gmail.com>
2020-04-15 15:53:32 +0200

    change ip

--- app/src/main/java/com/example/nianiadladziecka/AppConfig.java
+++ app/src/main/java/com/example/nianiadladziecka/AppConfig.java
@@ -3,14 +3,14 @@ package com.example.nianiadladziecka;
 public class AppConfig {
 
     //Rpi IP
-    public static String IP_RPI = "192.168.0.13";
+    public static String IP_RPI = "192.168.10.199";
 
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.13/babyMonitor/login.php";
+    public static String URL_LOGIN = "http://192.168.10.199/babyMonitor/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.13/babyMonitor/register.php";
+    public static String URL_REGISTER = "http://192.168.10.199/babyMonitor/register.php";
 
     // Camera url
-    public static String URL_CAMERA = "http://192.168.0.13:8080/stream";
+    public static String URL_CAMERA = "http://192.168.10.199:8080/stream";
 }
